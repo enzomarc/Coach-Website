b9c9259c23588e3a100567270f70b6f5a818ee3c
focus237 <emarc237@gmail.com>
2018-12-20 10:31:28 +0100

    added form helpers

--- app/helpers.php
+++ app/helpers.php
@@ -1,9 +1,22 @@
 <?php
 
-function input(string $key = null)
+function input(string $key = null): array
 {
     if (is_null($key))
-        return $_REQUEST;
+        return $_POST;
 
-    return $_REQUEST[$key];
+    return $_POST;
+}
+
+function except(string $key): array
+{
+    $input = [];
+    $all = input();
+
+    foreach($all as $k => $v) {
+        if ($k != $key)
+            $input[$k] = $v;
+    }
+
+    return $input;
 }
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,11 @@
 
 use App\Poison;
 
+require_once '../app/helpers.php';
+
 $poison = new Poison(dirname(__DIR__) . '/resources/views');
+
+Poison::setViewExtension('.poison.php');
 
 $poison->addGlobal('router', $router);
 
@@ -14,6 +18,14 @@ $router->get('/services', function () use($poison) {
     $poison->render('services');
 })->name('services');
 
+$router->get('/products', function () use($poison) {
+    $poison->render('products');
+})->name('products');
+
 $router->get('/contact', function () use($poison) {
     $poison->render('contact');
 })->name('contact');
+
+$router->post('/contact', function () {
+    var_dump(input());
+});
